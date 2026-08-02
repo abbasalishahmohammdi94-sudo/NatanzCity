27266ef4a5a8e27be375b26168b335da9aec9b92
abbasalishahmohammdi94-sudo <abbasali.shahmohammdi94@gmail.com>
2026-08-02 12:52:40 +0330

    add NatanzCity assets v0.3 stable

---
+++ assets/js/OrgJS/script.js
@@ -0,0 +1,45 @@
+const NatanzSwiper = new Swiper(".NatanzSwiper", {
+  loop: false,
+
+  grabCursor: true,
+
+  spaceBetween: 20,
+
+  speed: 800,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+    dragSize: "auto",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
