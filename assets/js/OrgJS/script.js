e950b55e20bb695f3f7546ec33bf0d0821fc13c8
abbasalishahmohammdi94-sudo <abbasali.shahmohammdi94@gmail.com>
2026-08-03 13:01:28 +0330

    update js
    ver0.4

--- assets/js/OrgJS/script.js
+++ assets/js/OrgJS/script.js
@@ -63,3 +63,36 @@ scrollBtn.addEventListener("click", () => {
     });
 
 });
+const texts = [
+    "در حال پیدا کردن کوچه‌های قدیمی نطنز... 🏛️",
+    "داریم مسجد جامع رو آماده می‌کنیم... 😄",
+    "در حال گردگیری تاریخ نطنز... ✨",
+    "تقریباً رسیدیم، چای رو هم آماده کنید ☕😂"
+];
+
+let i = 0;
+
+const loaderText = document.querySelector("#loader-text");
+const loader = document.querySelector("#loader");
+
+const textInterval = setInterval(() => {
+    loaderText.textContent = texts[i];
+    i = (i + 1) % texts.length;
+}, 700);
+
+
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+
+        clearInterval(textInterval);
+
+        loader.style.opacity = "0";
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 300);
+
+    }, 3000);
+
+}); 
