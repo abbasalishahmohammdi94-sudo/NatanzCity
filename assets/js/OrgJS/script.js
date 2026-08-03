e7c321ada05de732b2473ff6015625aa0028a568
abbasalishahmohammdi94-sudo <abbasali.shahmohammdi94@gmail.com>
2026-08-03 11:45:32 +0330

    add new js

--- assets/js/OrgJS/script.js
+++ assets/js/OrgJS/script.js
@@ -43,3 +43,23 @@ const NatanzSwiper = new Swiper(".NatanzSwiper", {
     },
   },
 });
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        scrollBtn.style.display = "block";
+    }else{
+        scrollBtn.style.display = "none";
+    }
+
+});
+
+scrollBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
